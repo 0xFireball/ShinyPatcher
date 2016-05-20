03e837ca97e414de7d9160a000fd6a71051d5a22
0xFireball <0xFireball@outlook.com>
2016-05-20 14:13:14 -0700

    add more windows

--- ZetaPatcher/ZetaPatcher/MainWindow.xaml.cs
+++ ZetaPatcher/ZetaPatcher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace ZetaPatcher
 {
@@ -21,6 +22,27 @@ namespace ZetaPatcher
         private void BtnZetaPhase_OnClick(object sender, RoutedEventArgs e)
         {
             Process.Start("https://zetaphase.io");
+        }
+
+        private void BtnGenerateSignature_OnClick(object sender, RoutedEventArgs e)
+        {
+            ShowChildWindow(new SigMaker());
+        }
+
+        private void btnCreatePatch_Click(object sender, RoutedEventArgs e)
+        {
+            ShowChildWindow(new PatchCreator());
+        }
+
+        private void btnApplyPatch_Click(object sender, RoutedEventArgs e)
+        {
+            ShowChildWindow(new PatchApplier());
+        }
+
+        private void ShowChildWindow(MetroWindow childWindow)
+        {
+            childWindow.Owner = this;
+            childWindow.ShowDialog();
         }
     }
 }
