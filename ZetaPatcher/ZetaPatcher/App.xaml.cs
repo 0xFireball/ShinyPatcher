0752d3784690d4647bdc50993afb411727fd1d41
0xFireball <0xFireball@outlook.com>
2016-05-20 14:03:44 -0700

    main window

---
+++ ZetaPatcher/ZetaPatcher/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ZetaPatcher
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ZetaPatcher/ZetaPatcher/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace ZetaPatcher
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnProjectPage_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://zetaphase.github.io/ZetaPatcher");
+        }
+
+        private void BtnZetaPhase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://zetaphase.io");
+        }
+    }
+}
